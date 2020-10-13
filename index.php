e72f7ca03420f1207fb784b231e4fbb06affd6f5
Kelpie <ariel.p20@mhs.istts.ac.id>
2020-10-13 13:20:55 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="style.css">
+
+<body>
+
+</body>
+<script src="./Project-PW/script.js"></script>
+
+</html>
