47bc82575abca8a0f25e0e200255bb49d6ceb761
Kelpie <ariel.p20@mhs.istts.ac.id>
2020-11-29 17:52:07 +0800

    fixing some stuff

--- LogReg_Form/Login.php
+++ LogReg_Form/Login.php
@@ -11,32 +11,34 @@
 </head>
 
 <body>
+    <a href='../header.php' style="text-align: left; font-size: 1.5rem;">
+        < back to homepage </a> <div class="container">
+            <div class="login-content">
+                <div class="login-image">
+                    <figure><img src="login.png" width="350px" height="auto" style="padding-top: 30px;"></figure>
+                </div>
 
-    <div class="container">
-        <div class="login-content">
-            <div class="login-image">
-                <figure><img src="login.png" width="400" height="300" style="padding-top: 30px;"></figure>
+                <div class="login-form">
+                    <h2 class="form-title"><b>Login</b></h2>
+                    <form method="POST" class="register-form" id="login-form">
+                        <div class="form-group">
+                            <label for="">Username</label>
+                            <input type="text" name="username" id="username" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Password</label>
+                            <input type="password" name="password" id="password" />
+                            <hr>
+                        </div>
+                        <div class="form-group form-button">
+                            <input type="submit" name="login" id="login" class="form-submit" value="Log in" />
+                            <a href="./register.php"><button type="button" class="form-submit" style="margin-left: 5px; margin-top: 16px;">Go to Register</button></a>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <div class="login-form">
-                <h2 class="form-title"><b>Login</b></h2>
-                <form method="POST" class="register-form" id="login-form">
-                    <div class="form-group">
-                        <input type="text" name="username" id="username" placeholder="Username" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="password" id="password" placeholder="Password" />
-                        <hr>
-                    </div>
-                    <div class="form-group form-button">
-                        <input type="submit" name="login" id="login" class="form-submit" value="Log in" />
-                        <a href="./register.php"><button type="button" class="form-submit" style="margin-left: 5px; margin-top: 16px;">Go to Register</button></a>
-                    </div>
-                </form>
             </div>
-        </div>
-    </div>
 </body>
 
 </html>
--- LogReg_Form/Register.php
+++ LogReg_Form/Register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,50 +10,61 @@
     <title>Register</title>
 
     <style>
-        .container{
+        .container {
             margin-top: 0px;
             height: 600px;
         }
+
+        body {
+            padding-top: 50px;
+        }
     </style>
 </head>
+
 <body>
 
     <div class="container">
         <div class="regis-content">
             <div class="regis-image">
-                <figure><img src="register.png" width="400" height="300" style="padding-top: 100px; margin-right: 100px;"></figure>
+                <figure><img src="register.png" width="300px" height="auto" style="padding-top: 100px; margin-right: 100px;"></figure>
             </div>
 
             <div class="regis-form">
-                <h2 class="form-title"><b>Register</b></h2>
+                <h2 class="form-title" style='font-size: 2.5rem; text-align: left;'><b>Register</b></h2>
                 <form method="post" class="register-form" id="login-form">
                     <div class="form-group">
-                        <input type="text" name="name" id="name" placeholder="Your Name"/>
+                        <label for="">Name</label>
+                        <input type="text" name="name" id="name" />
                         <hr>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="username" id="username" placeholder="Username"/>
+                        <label for="">Username</label>
+                        <input type="text" name="username" id="username" />
                         <hr>
                     </div>
                     <div class="form-group">
-                        <input type="text" name="email" id="email" placeholder="E-mail"/>
+                        <label for="">E-mail</label>
+                        <input type="text" name="email" id="email" />
                         <hr>
                     </div>
                     <div class="form-group">
-                        <input type="password" name="password" id="password" placeholder="Password"/>
+                        <label for="">Password</label>
+                        <input type="password" name="password" id="password" />
                         <hr>
-                    </div>                    
+                    </div>
                     <div class="form-group">
-                        <input type="password" name="conpass" id="conpass" placeholder="Confirm Password"/>
+                        <label for="">Confirm Password</label>
+                        <input type="password" name="conpass" id="conpass" />
                         <hr>
                     </div>
                     <div class="form-group form-button">
                         <input type="submit" name="register" class="form-submit" value="Register" style="margin-right: 5px;"></input>
-                        <a href="./login.php" ><button type="button" class="form-submit">Go to Login</button></a>
+                        <a href="./login.php"><button type="button" class="form-submit">Go to Login</button></a>
                     </div>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
