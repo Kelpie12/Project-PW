f6eb6282c80eb471aca54f1168dab4af8d7c7302
Kelpie <ariel.p20@mhs.istts.ac.id>
2020-12-02 19:06:04 +0800

    small change

--- LogReg_Form/Register.php
+++ LogReg_Form/Register.php
@@ -31,48 +31,49 @@ include("../public/register-nav.php");
 </head>
 
 <body>
-    <div class="container">
-        <div class="regis-content">
-            <div class="regis-image">
-                <figure><img src="register.png" width="400" height="300" style="padding-top: 125px; margin-right: 100px;"></figure>
-            </div>
+    <a href="../header.php" style="text-align:left; font-size: 1.1em;">
+        < Back To Homepage</a> <div class="container">
+            <div class="regis-content">
+                <div class="regis-image">
+                    <figure><img src="register.png" width="400" height="300" style="padding-top: 125px; margin-right: 100px;"></figure>
+                </div>
 
-            <div class="regis-form">
-                <h2><b>Register</b></h2>
-                <form method="post" class="register-form" id="login-form">
-                    <div class="form-group">
-                        <input type="text" name="register_fname" id="register_fname" style="width: 140px;" placeholder="First Name" />
-                        <input type="text" name="register_lname" id="register_lname" style="width: 140px;" placeholder="Last Name" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="register_username" id="register_username" style="width: 140px;" placeholder="Username" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="register_password" id="register_password" placeholder="Password" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="password" name="register_confirm" id="register_confirm" placeholder="Confirm Password" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="register_email" id="register_email" placeholder="E-mail" />
-                        <hr>
-                    </div>
-                    <div class="form-group">
-                        <input type="date" name="register_birthdate" id="register_birthdate" placeholder="mm/dd/yyyy" />
-                        <hr>
-                    </div>
-                    <div class="form-group form-button">
-                        <input type="submit" name="register_btn" class="form-submit" value="register" style="margin-right: 5px;"></input>
-                        <a href="./login.php"><button type="button" class="form-submit">Go to Login</button></a>
-                    </div>
-                </form>
+                <div class="regis-form">
+                    <h2><b>Register</b></h2>
+                    <form method="post" class="register-form" id="login-form">
+                        <div class="form-group">
+                            <input type="text" name="register_fname" id="register_fname" style="width: 140px;" placeholder="First Name" />
+                            <input type="text" name="register_lname" id="register_lname" style="width: 140px;" placeholder="Last Name" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="register_username" id="register_username" style="width: 140px;" placeholder="Username" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="register_password" id="register_password" placeholder="Password" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="register_confirm" id="register_confirm" placeholder="Confirm Password" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="register_email" id="register_email" placeholder="E-mail" />
+                            <hr>
+                        </div>
+                        <div class="form-group">
+                            <input type="date" name="register_birthdate" id="register_birthdate" placeholder="mm/dd/yyyy" />
+                            <hr>
+                        </div>
+                        <div class="form-group form-button">
+                            <input type="submit" name="register_btn" class="form-submit" value="register" style="margin-right: 5px;"></input>
+                            <a href="./login.php"><button type="button" class="form-submit">Go to Login</button></a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
-    </div>
+            </div>
 </body>
 
 </html>
