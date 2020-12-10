074f8dee207981cecdd6a60f84a2b38ac4815210
blastersun <felixlung222@gmail.com>
2020-12-10 15:59:59 -0800

    upload_ulang
    modif login.php dan register.php

--- LogReg_Form/Login.php
+++ LogReg_Form/Login.php
@@ -7,8 +7,14 @@ include("../public/login-nav.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/Semantic-UI-CSS-master/semantic.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous">
+    </script>
+    <script src="assets/Semantic-UI-CSS-master/semantic.min.js"></script>
 
     <title>Login</title>
     <style>
@@ -34,33 +40,25 @@ include("../public/login-nav.php");
 <body>
     <a href="../header.php" style="text-align:left; font-size: 1.1em;">
         < Back To Homepage</a> <div class="container">
-            <div class="login-content">
-                <div class="login-image">
-                    <figure><img src="login.png" width="400" height="300" style="padding-top: 30px;"></figure>
-                </div>
-
-                <div class="login-form">
+        <div class="login-content">
+            <div class="login-image">
+                <figure><img src="login.png" width="400" height="300" style="padding-top: 30px;"></figure>
+            </div>
+            <div class="login-form">
+                <form method="POST" class="ui form" id="login-form" style="padding-top: 110px;">
                     <h2><b>Login</b></h2>
-                    <form method="POST" class="register-form" id="login-form">
-                        <div class="form-group">
-                            <input type="text" name="login_username" id="login_username" placeholder="Username" />
-                            <hr>
-                        </div>
-                        <div class="form-group">
-                            <input type="password" name="login_password" id="login_password" placeholder="Password" />
-                            <hr>
-                        </div>
-                        <div class="form-group">
-                            <a href="forget-password.php" style='text-align: left;'>forget my password</a>
-                        </div>
-                        <div class="form-group form-button">
-                            <input type="submit" name="login_btn" id="login" class="form-submit" value="Log in" />
-                            <a href="./register.php"><button type="button" class="form-submit" style="margin-left: 5px; margin-top: 16px;">Go to Register</button></a>
-                        </div>
-                    </form>
-                </div>
+                    <div class="field">
+                        <input type="text" name="Login_username" id="Login_username" placeholder="Username"/>
+                    </div>
+                    <div class="field">
+                        <input type="password" name="Login_password" id="Login_password" placeholder="Password"/>
+                    </div>
+                    <button type="submit" name="Login_btn" id="login" class="ui button">Login</button>
+                    <a href="./Register.php" type="button" class="ui button" style="margin-left: 5px;">Go to Register</a>  
+                </form>
             </div>
-            </div>
+        </div>
+    </div>
 </body>
 
 </html>
